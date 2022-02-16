0049a4264461b418e0e45e3c611ccda22cb4c306
Your Name <you@example.com>
2022-02-16 16:26:37 +0100

    Commit

--- main.js
+++ main.js
@@ -7,3 +7,21 @@ const listItem = famousSayings[0];
 console.log(listItem);
 console.log(famousSayings[2]);
 console.log(famousSayings[3]);
+//Update Elements
+let groceryList = ['bread', 'tomatoes', 'milk'];
+groceryList[1] = 'avocados';
+console.log(groceryList);
+//Arrays with let and const
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha'];
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork'];
+
+condiments[0] = 'Mayo';
+console.log(condiments);
+
+condiments = ['Mayo'];
+utensils[3] = 'Spoon';
+console.log(utensils);
+
+//The .length property
+const objectives = ['Learn a new languages', 'Read 52 books', 'Run a marathon'];
+console.log(objectives.length);
