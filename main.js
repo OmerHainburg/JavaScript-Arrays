475bf23926126574a56dabadbc0dac2aa2fa8f70
Your Name <you@example.com>
2022-02-16 11:56:45 +0100

    Commit

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 //Create An Array
 const hobbies = ['omer', 'farooq', 'hashim']
 console.log(hobbies);
+//Accessing Elements
+const famousSayings = ['Fortune favors the brave.', 'A joke is a very serious thing.', 'Where there is love there is life.'];
+const listItem = famousSayings[0];
+console.log(listItem);
+console.log(famousSayings[2]);
+console.log(famousSayings[3]);
